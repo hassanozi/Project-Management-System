a900dd5945c6b69af66f0206986d8795976dee96
MaryemHaytham <maryemhaythamm@gmail.com>
2024-03-18 05:12:59 +0200

    Login design done

--- src/app/core/core-routing.module.ts
+++ src/app/core/core-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CoreComponent } from './core.component';
+import { LoginComponent } from './Components/login/login.component';
 
-const routes: Routes = [{ path: '', component: CoreComponent }];
+const routes: Routes = [
+  { path: '', component: LoginComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,15 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { CoreRoutingModule } from './core-routing.module';
 import { CoreComponent } from './core.component';
+import { LoginComponent } from './Components/login/login.component';
+import { SharedModule } from '../shared/shared.module';
+
+
 
 
 @NgModule({
   declarations: [
-    CoreComponent
+    CoreComponent,
+    LoginComponent,
+    
   ],
   imports: [
     CommonModule,
-    CoreRoutingModule
+    CoreRoutingModule,
+    SharedModule
   ]
 })
 export class CoreModule { }
