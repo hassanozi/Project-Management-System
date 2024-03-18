8426411835ae5be3c7f7f390fef8f3c7c60dc11f
Hassan <hassan.abdelrazek.hussien@outlook.com>
2024-03-18 07:32:34 +0200

    add dashboard and shared Modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   
-  { path: 'manager', loadChildren: () => import('./core/Modules/manager/manager.module').then(m => m.ManagerModule) },
-  { path: 'employee', loadChildren: () => import('./core/Modules/employee/employee.module').then(m => m.EmployeeModule) },
+  {path : '', redirectTo:'core' , pathMatch: 'full'},
   
   { path: 'core', loadChildren: () => import('./core/core.module').then(m => m.CoreModule) },
+  
+  { path: 'dashboard', loadChildren: () => import('./core/Modules/dashboard/dashboard.module').then(m => m.DashboardModule) },
   
   
 ];
