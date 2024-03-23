27e2c1816d1312b1d26e7904f16fa219ae966de1
MaryemHaytham <maryemhaythamm@gmail.com>
2024-03-23 05:51:29 +0200

    View, Pagination

--- src/app/core/Modules/manager/manager-users/manager-users.component.ts
+++ src/app/core/Modules/manager/manager-users/manager-users.component.ts
@@ -3,6 +3,8 @@ import { UsersService } from '../services/users.service';
 import { ConfirmBlockUserComponent } from './components/confirm-block-user/confirm-block-user.component';
 import { MatDialog } from '@angular/material/dialog';
 import { ToastrService } from 'ngx-toastr';
+import { ViewUserComponent } from './components/view-user/view-user.component';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-manager-users',
@@ -13,6 +15,11 @@ export class ManagerUsersComponent implements OnInit{
   tableResponse:any;
   userTable:any;
   users :any[]=[];
+  length = 50;
+  pageSize = 5;
+  pageIndex = 1;
+  pageSizeOptions = [5, 10, 25];
+  pageEvent: PageEvent |any;
 
 
   constructor(private _UsersService:UsersService,private dialog:MatDialog, private _ToastrService:ToastrService){}
@@ -21,16 +28,22 @@ export class ManagerUsersComponent implements OnInit{
       this.getAllUsers();
   }
 
+  handlePageEvent(e: PageEvent) {
+    this.pageSize = e.pageSize;
+    this.pageIndex = e.pageIndex;
+    this.getAllUsers();
+  }
+
   getAllUsers(){
     let paramData = {
-      pageSize :10,
-      pageNumber :1
+      pageSize:this.pageSize,
+      pageNumber:this.pageIndex,
 
     }
     this._UsersService.getUsers(paramData).subscribe({
       next:(res)=>{
-        console.log(res);
-        this.userTable = res
+        console.log(res.pageSize);
+        this.tableResponse = res
         this.users = res.data
       }
 
@@ -66,6 +79,22 @@ export class ManagerUsersComponent implements OnInit{
         this._ToastrService.success('Status Changed Successfuly')
       }
     })
+  }
+
+  openUserDetailes(item:any){
+    const dialogRef = this.dialog.open(ViewUserComponent, {
+      data:item
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      console.log(result)
+      if(result){
+        
+        
+      }
+    });
+
   }
 
 
