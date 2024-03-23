e95c79c2ce6a6e2eb58de12b88e3ea3d16acfaee
Hassan <hassan.abdelrazek.hussien@outlook.com>
2024-03-23 14:14:59 +0200

    Tasks-list

---
+++ src/app/core/Modules/manager/services/tasks.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TasksService {
+
+  constructor(private _HttpClient: HttpClient) { }
+
+  getManagerTasks(param: any): Observable<any> {
+    return this._HttpClient.get('Task/manager', { params: param })
+  }
+
+  // onAddProject(data: FormGroup): Observable<any> {
+  //   return this._HttpClient.post('Project', data)
+  // }
+
+  // onDeleteProject(id: number): Observable<any> {
+  //   return this._HttpClient.delete(`Project/${id}`)
+  // }
+
+  // getProjectById(id: number): Observable<any> {
+  //   return this._HttpClient.get(`Project/${id}`)
+  // }
+
+}
