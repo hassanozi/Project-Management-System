7cd36df10281e8647fc631a582fc57e4d68eb7bd
AhmedYasser-js <ahmedkasso22@gmail.com>
2024-03-24 02:22:03 +0200

    "creat sidebar"

--- src/app/core/Modules/dashboard/dashboard.component.ts
+++ src/app/core/Modules/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { SidebarComponent } from 'src/app/shared/components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent {
+  @ViewChild(SidebarComponent) sidebar: SidebarComponent | undefined;
 
 }
